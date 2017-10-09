a63916570cb706e2de85b269a498078006184a0f
John  Lee <johnlee@Johns-MacBook-Pro.local>
2017-10-09 16:51:23 -0700

    Add form to create new question and display questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,27 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
+  end
+
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @user = User.find(1) # pass user session id 
+    @question = @user.questions.new(question_params)
+    @question.save
+   
+    redirect_to questions_path
   end
 
   def show
+    @question = Question.find(params[:id])
   end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root 'questions#index'
 
-  resources 'questions', only: [:new, :create, :update, :destroy]
+  resources 'questions'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
